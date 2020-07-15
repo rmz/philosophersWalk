5a82890155d67b4d49619d65201e83daeb786bc1
Ricardo Miranda Zuniga <ricardo@ambriente.com>
2020-07-15 09:44:36 -0400

    Add files via upload

---
+++ Assets/Scripts/LoadLevelAfterTime.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadLevelAfterTime : MonoBehaviour
+{
+    [SerializeField]
+    private float delayBeforeLoading = 10f;
+    [SerializeField]
+    private string sceneNameToLoad;
+
+    private float timeElapsed;
+
+    // Update is called once per frame
+    private void Update()
+    {
+        //load next scene in background - load immediately
+        //AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("Environment");
+        timeElapsed += Time.deltaTime;
+        if (timeElapsed > delayBeforeLoading)
+        {
+                //SceneManager.LoadScene(int 1);
+                SceneManager.LoadScene(sceneNameToLoad);
+        }
+    }
+}
